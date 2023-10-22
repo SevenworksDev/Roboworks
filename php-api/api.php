1e1d7fd5309c352de8a10d5f20b0d03a847bc737
Sevenworks <91027492+SevenworksDev@users.noreply.github.com>
2023-10-21 22:45:28 -0700

    Update api.php

--- php-api/api.php
+++ php-api/api.php
@@ -1,38 +1,5 @@
 <?php
-$api = '';
-
-function generateUUID() {
-    $UUID = '';
-    $chars = 'abcdef0123456789';
-    for ($i = 0; $i < 32; $i++) {
-        $charIndex = mt_rand(0, strlen($chars) - 1);
-        $UUID .= $chars[$charIndex];
-        if ($i === 7 || $i === 11 || $i === 15 || $i === 19) {
-            $UUID .= '-';
-        }
-    }
-    return $UUID;
-}
-
-function ai($uuid, $message) {
-    $url = $api."/{$uuid}?q=".urlencode($message);
-
-    $ch = curl_init($url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    $response = curl_exec($ch);
-    curl_close($ch);
-    $data = json_decode($response, true);
-
-    if ($data && isset($data[0]['response'][count($data[0]['response']) - 1]['content']['response'])) {
-        $rwresponse = $data[0]['response'][count($data[0]['response']) - 1]['content']['response'];
-        echo $rwresponse;
-    } else {
-        echo '[Roboworks]: Failed to retrieve response.';
-    }
-}
-
-$uuid = generateUUID();
-$message = $_POST['q'];
-
-ai($uuid, $message);
+# bro is speaking facts not gonna lie
+$q = $_GET['q'] ?? die('Missing q parameter')
+echo file_get_contents('<put your cloudflare worker link here smartypants>' . substr(str_shuffle('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'), 0, 24) . "?q=" . urlencode($q)) ?? die('Roboworks failed to respond.');
 ?>

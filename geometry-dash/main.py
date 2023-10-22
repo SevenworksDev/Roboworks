7795070e9f9afe39ef2ec3c86335caf665429ad5
Sevenworks <91027492+SevenworksDev@users.noreply.github.com>
2023-10-22 11:19:08 -0700

    Update main.py

--- geometry-dash/main.py
+++ geometry-dash/main.py
@@ -1,4 +1,4 @@
-import time, requests, random, base64, hashlib, urllib.parse, emoji
+import time, requests, random, base64, hashlib, urllib.parse, re
 from itertools import cycle
 from json import loads
 from threading import Thread
@@ -11,12 +11,19 @@ accountID = ""
 llamaURL = ""
 # CONFIG END
 
-
-def process_llama(data):
-    if any(char in data for char in emoji.UNICODE_EMOJI):
-        raise ValueError('Invalid AI Response')
-    if "\n" in response:
-        raise ValueError('Invalid AI Response')
+emoji = re.compile("["
+                           u"\U0001F600-\U0001F64F"  # Emoticons
+                           u"\U0001F300-\U0001F5FF"  # Miscellaneous Symbols and Pictographs
+                           u"\U0001F680-\U0001F6FF"  # Transport and Map Symbols
+                           u"\U0001F700-\U0001F77F"  # Alchemical Symbols
+                           u"\U0001F780-\U0001F7FF"  # Geometric Shapes Extended
+                           u"\U0001F800-\U0001F8FF"  # Supplemental Arrows-C
+                           u"\U0001F900-\U0001F9FF"  # Supplemental Symbols and Pictographs
+                           u"\U0001FA00-\U0001FA6F"  # Extended-A
+                           u"\U0001FA70-\U0001FAFF"  # Extended-B
+                           u"\U0001F004-\U0001F0CF"  # Mahjong Tiles
+                           u"\U0001F200-\U0001F251"  # Enclosed Ideographic Supplement
+                           "]+", flags=re.UNICODE)
 
 def comment_chk(*,username,comment,levelid,percentage,type):
     part_1 = username + comment + levelid + str(percentage) + type + "xPT6iUrtws0J"
@@ -47,26 +54,29 @@ def comment(name,passw,comment,perc,level):
         except:
                 return "problem"
 
+
 def llama(level):
     url = f"http://gdbrowser.com/api/comments/{level}?count=1"
     r = loads(requests.get(url).text)[0]
     u, com = r['username'], r['content']
 
-    if com.lower().startswith("@digital"):
+    if com.lower().startswith("/llama"):
         try:
             print(f"{u}: {com}")
+            time.sleep(12)
             llamaUUID = ''.join(random.choice("0123456789") for i in range(12))
             response = requests.get(f"{llamaURL}/{llamaUUID}?q={urllib.parse.quote(com[5:])}").text
-            process_llama(response)
-            comment(username, password, f"@{u} {response}", "0", level)
+            response.replace("\n", " ")
+            result = emoji.sub('', response)
+            comment(username, password, f"@{u} {result}", "0", level)
         except Exception as e:
             print(str(e))
 
-print("Bot is on")
+print("Roboworks is on")
 while True:
     try:
         t = Thread(target=llama, args=(levelID,))
         t.start()
-        time.sleep(2)
+        time.sleep(1.5)
     except:
         print("err")
